097fb90926915732154ab8dc9bf474a9f2bd114f
William Lavoie <144185513+William-Lavoie@users.noreply.github.com>
2025-09-16 19:57:17 -0400

    OTP - generation of otp with pyopt

--- brokerX/broker/infrastructure/email_otp_manager.py
+++ brokerX/broker/infrastructure/email_otp_manager.py
@@ -1,10 +1,18 @@
 from django.core.mail import send_mail
+import pyotp
 
 def send_email_passcode(email: str):
+    passcode = generate_passcode()
     send_mail(
     "Subject here",
-    "Here is the message.",
+    passcode.now(),
     "from@example.com",
     [email],
     fail_silently=False,
 )
+
+def generate_passcode() -> pyotp.TOTP:
+    user_secret = pyotp.random_base32()
+    return pyotp.TOTP(s=user_secret,
+                          interval=600,
+                          digits=6)
