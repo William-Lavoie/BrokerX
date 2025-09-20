c860f62e500ae8eef4d1ca5914870d144cda7a08
William Lavoie <144185513+William-Lavoie@users.noreply.github.com>
2025-09-20 12:08:54 -0400

    fix migration history and add uniqueness to email in user

--- brokerX/broker/models.py
+++ brokerX/broker/models.py
@@ -8,7 +8,7 @@ class User(models.Model):
     last_name = models.CharField(max_length=100)
     address = models.CharField(max_length=100)
     birth_date = models.DateField()
-    email = models.EmailField()
+    email = models.EmailField(unique=True)
     phone_number = models.CharField(max_length=100)
     status = models.CharField(max_length=20, choices=[("A", "Active"),
                                                       ("P", "Pending"),
