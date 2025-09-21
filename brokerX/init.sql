7ef057e3129db95890309ebf23b96a62db2599d3
William Lavoie <144185513+William-Lavoie@users.noreply.github.com>
2025-09-21 14:52:49 -0400

    CI - removing init file after all

--- brokerX/init.sql
+++
@@ -1,5 +0,0 @@
--- Used to give permissions to the runner in the CI script
-CREATE DATABASE IF NOT EXISTS test_brokerX_db;
-
-GRANT ALL PRIVILEGES ON test_brokerX_db.* TO 'user'@'%';
-FLUSH PRIVILEGES;
