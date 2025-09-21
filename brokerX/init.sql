74c7e385775ddd0775795a1ca9ef9b31c278aef8
William Lavoie <144185513+William-Lavoie@users.noreply.github.com>
2025-09-21 14:43:55 -0400

    CI - removing user creation from init file

--- brokerX/init.sql
+++ brokerX/init.sql
@@ -1,6 +1,5 @@
 -- Used to give permissions to the runner in the CI script
 CREATE DATABASE IF NOT EXISTS test_brokerX_db;
 
-CREATE USER IF NOT EXISTS 'user'@'%' IDENTIFIED BY 'pass';
 GRANT ALL PRIVILEGES ON test_brokerX_db.* TO 'user'@'%';
 FLUSH PRIVILEGES;
