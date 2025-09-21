763e278adfed3db5b5a7bced72aed10248d7ddda
William Lavoie <144185513+William-Lavoie@users.noreply.github.com>
2025-09-21 14:41:55 -0400

    CI - giving persmissions only to test database

--- brokerX/init.sql
+++ brokerX/init.sql
@@ -1,8 +1,6 @@
 -- Used to give permissions to the runner in the CI script
-CREATE DATABASE IF NOT EXISTS brokerX_db;
 CREATE DATABASE IF NOT EXISTS test_brokerX_db;
 
 CREATE USER IF NOT EXISTS 'user'@'%' IDENTIFIED BY 'pass';
-GRANT ALL PRIVILEGES ON brokerX_db.* TO 'user'@'%';
 GRANT ALL PRIVILEGES ON test_brokerX_db.* TO 'user'@'%';
 FLUSH PRIVILEGES;
