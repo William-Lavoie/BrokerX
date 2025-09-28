7a636fec4ddd93a628b9726cf64fa347db01c6bd
William Lavoie <144185513+William-Lavoie@users.noreply.github.com>
2025-09-28 14:43:48 -0400

    script CD et deploy

---
+++ brokerX/deploy.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+set -euo pipefail
+
+# Pull code
+git pull origin main
+
+# Start Docker containers
+docker compose down -v
+docker compose build --no-cache
+docker compose up -d --remove-orphans
+
+# Cleanup docker ressources
+docker image prune -f
+docker container prune -f
+docker volume prune -f
+docker network prune -f
+docker builder prune -af
+
+echo "Deployment completed successfully."
+echo "BrokerX is available on port 8000"
