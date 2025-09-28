80871e897dab4f303194831d4a6488db5347815b
William Lavoie <144185513+William-Lavoie@users.noreply.github.com>
2025-09-28 11:28:08 -0400

    dockerizing the app

---
+++ brokerX/entrypoint.py
@@ -0,0 +1,23 @@
+import sys
+import time
+
+from django.core.management import execute_from_command_line
+from django.db import connections
+from django.db.utils import OperationalError
+
+
+def wait_for_db(retries=10, delay=3):
+    for i in range(retries):
+        try:
+            connection = connections["default"]
+            connection.ensure_connection()
+            return
+        except OperationalError:
+            time.sleep(delay)
+    sys.exit(1)
+
+
+if __name__ == "__main__":
+    wait_for_db()
+    execute_from_command_line(["manage.py", "migrate"])
+    execute_from_command_line(["manage.py", "runserver", "0.0.0.0:8000"])
