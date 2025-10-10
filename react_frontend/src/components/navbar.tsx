59250da732e381a28b39ac6a6dfe7b78d337ec9c
William Lavoie <144185513+William-Lavoie@users.noreply.github.com>
2025-10-09 22:13:26 -0400

    slowly making the react ui

--- react_frontend/src/components/navbar.tsx
+++ react_frontend/src/components/navbar.tsx
@@ -1,40 +1,30 @@
-"use client"
-import { useEffect, useState } from "react";
-
+/* eslint-disable @next/next/no-img-element */
 function NavbarButton({text, route}: {text: string, route: string}) {
     return (
-        <a href={route} className="w-1/10 flex justify-center items-center hover:bg-blue-500 cursor-pointer">{text}</a>
-    )
+        <a href={route} className="w-1/10 flex justify-center items-center hover:bg-blue-500 cursor-pointer">
+            {text}
+        </a>
+    );
 }
 
 export function Navbar() {
-    const [windowWidth, setWindowWidth] = useState(window.innerWidth);
-
-    useEffect(() => {
-        const handleResize = () => {
-        setWindowWidth(window.innerWidth);
-        };
-
-        window.addEventListener('resize', handleResize);
-
-        return () => {
-        window.removeEventListener('resize', handleResize);
-        };
-    }, []);
-    
     return (
-        <> {windowWidth < 768 ? 
-                <nav className="h-[10vh] flex gap-x-[3%] sticky top-0">
-                    <NavbarButton text="Home" route="/"></NavbarButton>
-                    <NavbarButton text="Wallet" route="/wallet"></NavbarButton>
-                    <NavbarButton text="Place Order" route="/create_account"></NavbarButton>
-                    <NavbarButton text="Logout" route="/login"></NavbarButton>
-                </nav>
-            ?
-            <div>Ok</div>
-        }
+        <>
+            <nav className="hidden md:flex bg-blue-300 h-[10vh] gap-x-[3%] sticky top-0">
+                <img src="/images/default.png"></img>
+                <NavbarButton text="Home" route="/" />
+                <NavbarButton text="Wallet" route="/wallet" />
+                <NavbarButton text="Place Order" route="/create_account" />
+                <NavbarButton text="Logout" route="/login" />
+            </nav>
+
+            <nav className="md:hidden bg-blue-300 flex h-[10vh] justify-between pr-[10px] sticky top-0">
+                <img src="/images/profile.png"></img>
+                <button>
+                    <img src="/images/dropdown.svg" alt="Dropdown menu" className="w-[50px]"/>
+                </button>
+            </nav>
+
         </>
-           
-      
-    )
+    );
 }
