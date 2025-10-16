0c2ff792e793938833b3bbd1fcb6d46e5ad89efc
William-Lavoie <william.lavoie.3@ens.etsmtl.ca>
2025-10-16 11:45:07 -0400

    login and logout from keyclock

--- react_frontend/src/app/page.tsx
+++ react_frontend/src/app/page.tsx
@@ -1,17 +1,30 @@
-"use client"
+"use client";
 
 import { useEffect, useState } from "react";
+import { keycloak } from "@/app/keycloak";
 
 export default function Home() {
-    const [data, setData] = useState("Your username");
+  const [username, setUsername] = useState("Loading...");
 
-    useEffect(() => {
-      fetch('http://localhost:8000/get_name/')
-        .then(response => response.json())
-        .then(json => setData(json["name"]))
-        .catch(error => console.error(error));
-    }, []);
+  useEffect(() => {
+
+    keycloak.init({ onLoad: "login-required",  checkLoginIframe: false, }).then((authenticated) => {
+      if (authenticated) {
+
+        const parsedToken = keycloak.tokenParsed;
+        const preferredUsername = parsedToken?.preferred_username || "Unknown user";
+        setUsername(preferredUsername);
+      } else {
+        console.warn("User is not authenticated");
+      }
+    }).catch((err) => {
+      console.error("Keycloak init error:", err);
+    });
+  }, []);
+
   return (
-    <>{data}</>
+    <main>
+      <h1>Welcome, {username}</h1>
+    </main>
   );
 }
