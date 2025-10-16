6be0fba89eda20c5971d1a28656fbbe2ba5da05b
William-Lavoie <william.lavoie.3@ens.etsmtl.ca>
2025-10-16 13:32:00 -0400

    dockering keycloack with configuration

--- react_frontend/src/app/keycloak.tsx
+++ react_frontend/src/app/keycloak.tsx
@@ -1,7 +1,7 @@
 import Keycloak from "keycloak-js";
 
 export const keycloak = new Keycloak({
-  url: "http://localhost:8080",
+  url: "http://localhost:7080",
   realm: "BrokerX",
   clientId: "react-brokerX",
 });
