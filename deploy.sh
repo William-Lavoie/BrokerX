a0164f1e648ad76773976c83ff9b581dd5639944
William Lavoie <144185513+William-Lavoie@users.noreply.github.com>
2025-10-27 22:44:07 -0400

    migrations on docker up

--- deploy.sh
+++ deploy.sh
@@ -33,6 +33,7 @@ for SERVICE in "${SERVICES[@]}"; do
     docker compose down -v --remove-orphans
     docker compose build --no-cache
     docker compose up -d
+    docker compose run --rm "$SERVICE" python manage.py migrate
 
     echo -e "\033[0;32m$SERVICE deployed successfully.\033[0m"
     cd ".."
