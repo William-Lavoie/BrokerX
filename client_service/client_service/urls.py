73956a928e23df1bef1df70cb971b0f103e71fc0
William Lavoie <144185513+William-Lavoie@users.noreply.github.com>
2025-10-28 21:17:31 -0400

    end of phase 2

--- client_service/client_service/urls.py
+++ client_service/client_service/urls.py
@@ -15,10 +15,18 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 
+import os
+
 from client.api.client_view import MyTokenObtainPairView
 from django.contrib import admin
-from django.urls import include, path
+from django.urls import include, path, re_path
+from django.views.static import serve as static_serve
 from rest_framework_simplejwt.views import TokenRefreshView
+
+from . import settings
+
+swagger_ui_path = os.path.join(settings.BASE_DIR, "dist")
+openapi_yaml_path = settings.BASE_DIR
 
 urlpatterns = [
     path("client", include(("client.urls", "client"), namespace="client")),
@@ -26,4 +34,28 @@ urlpatterns = [
     path("api/token/", MyTokenObtainPairView.as_view(), name="token_obtain_pair"),
     path("api/token/refresh/", TokenRefreshView.as_view(), name="token_refresh"),
     path("admin/", admin.site.urls),
+    path(
+        "api/",
+        static_serve,
+        {
+            "path": "index.html",
+            "document_root": swagger_ui_path,
+        },
+    ),
+    path(
+        "api/openapi.yaml",
+        static_serve,
+        {
+            "path": "openapi.yaml",
+            "document_root": openapi_yaml_path,
+        },
+    ),
+    # Serve all other static Swagger UI files under /api/
+    re_path(
+        r"^api/(?P<path>.+)$",
+        static_serve,
+        {
+            "document_root": swagger_ui_path,
+        },
+    ),
 ]
